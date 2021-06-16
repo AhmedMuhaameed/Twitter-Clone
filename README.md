f2cbfec86fe84554616182c7542dfe5f8c369732
AhmedMuhaameed <ahmed.m.abdelrahman0@gmail.com>
2021-06-16 18:57:53 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Twitter Project
+
+## Project Setup
+
+* install the dependencies - `npm install`
+* Run Project - `npm run start`
+
+## Project description 
+
+It's a simple clone of twitter using React and Redux.
---
+++ src/components/App.js
@@ -0,0 +1,39 @@
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import LoadingBar from "react-redux-loading";
+import { handleInitialData } from "../actions/shared";
+import Dashboard from "./Dashboard";
+import NewTweet from "./NewTweet";
+import TweetPage from "./TweetPage";
+import Nav from "./Nav";
+class App extends Component {
+    componentDidMount() {
+        this.props.dispatch(handleInitialData());
+    }
+    render() {
+        return (
+            <Fragment>
+                <LoadingBar />
+                <Router>
+                    <div className="container">
+                        <Nav />
+                        {this.props.loading === true ? null : (
+                            <div>
+                                <Route path="/" exact component={Dashboard} />
+                                <Route path="/tweets/:id" component={TweetPage} />
+                                <Route path="/new" component={NewTweet} />
+                            </div>
+                        )}
+                    </div>
+                </Router>
+            </Fragment>
+        );
+    }
+}
+function mapStateToProps({ authedUser }) {
+    return {
+        loading: authedUser === null,
+    };
+}
+export default connect(mapStateToProps)(App);
